7e01a71811066303974c05de04bad7af4ecca9cf
Tsiry <hei.tsiry@gmail.com>
2025-07-31 11:24:29 +0300

    add methode in Expense

--- src/main/java/antonio/geek/Entity/Expense.java
+++ src/main/java/antonio/geek/Entity/Expense.java
@@ -4,8 +4,8 @@ import java.time.LocalDate;
 
 public class Expense {
     private final String label;
-    double amount;
-    LocalDate date;
+    private double amount;
+    private LocalDate date;
 
     public Expense(String label, double amount, LocalDate date) {
         this.label = label;
@@ -23,5 +23,17 @@ public class Expense {
 
     public LocalDate getDate() {
         return date;
+    }
+
+    boolean isLargeExpense(){
+        return amount > 100;
+    }
+
+    public String toString(){
+        if(isLargeExpense()){
+
+            return this.label + " " + this.amount + " " + this.date;
+        }
+        return "erreur le montant doit etre supperieur a 100";
     }
 }
